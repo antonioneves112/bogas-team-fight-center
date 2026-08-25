1ac8fe1af1022b324ae9594199dc27a3d1a5e43a
António Neves <antonio.carlos.cosme.boturao@gmail.com>
2026-08-25 01:40:41 +0100

    sw update 101

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // =========================================================================
 // 🥊 MOTOR DA APP (SERVICE WORKER) - BOGAS TEAM - V14
 // =========================================================================
-const CACHE_NAME = "bogas-team-v100";
+const CACHE_NAME = "bogas-team-v101";
 
 const ASSETS_TO_CACHE = [
   "./",
